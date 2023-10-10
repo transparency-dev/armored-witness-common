98776131d78877f0254d68a69da9b85419b36f34
Al Cutter <al@google.com>
2023-10-10 17:09:50 +0100

    Refactor Scan

--- release/firmware/update/fetch.go
+++ release/firmware/update/fetch.go
@@ -267,57 +267,37 @@ func (f *Fetcher) Scan(ctx context.Context) error {
 
 		switch manifest.Component {
 		case ftlog.ComponentOS:
-			// According to the SemVer2.0 spec, equal revisions have no precedence defined.
-			// In general this won't be an issue; production releases will always be tagged appropriately,
-			// and there should never be two different releases with the same semver for a given component,
-			// however, during development, this may not hold.
-			// To tighten the definition of precedence, we'll use the fact that logs define ordering
-			// and say that "later" entries take precedence over "earlier" entries with the same version
-			// numbering.
-			if f.latestOS == nil ||
-				f.latestOS.manifest.GitTagName.LessThan(manifest.GitTagName) ||
-				f.latestOS.manifest.GitTagName.Equal(manifest.GitTagName) {
-				f.latestOS = &firmwareRelease{
-					bundle:   bundle,
-					manifest: manifest,
-				}
-			}
+			f.latestOS = highestRelease(f.latestOS, &firmwareRelease{bundle: bundle, manifest: manifest})
 		case ftlog.ComponentApplet:
-			// See comment above about the Equal case.
-			if f.latestApplet == nil ||
-				f.latestApplet.manifest.GitTagName.LessThan(manifest.GitTagName) ||
-				f.latestApplet.manifest.GitTagName.Equal(manifest.GitTagName) {
-				f.latestApplet = &firmwareRelease{
-					bundle:   bundle,
-					manifest: manifest,
-				}
-			}
+			f.latestApplet = highestRelease(f.latestApplet, &firmwareRelease{bundle: bundle, manifest: manifest})
 		case ftlog.ComponentBoot:
-			// See comment above about the Equal case.
-			if f.latestBoot == nil ||
-				f.latestBoot.manifest.GitTagName.LessThan(manifest.GitTagName) ||
-				f.latestBoot.manifest.GitTagName.Equal(manifest.GitTagName) {
-				f.latestBoot = &firmwareRelease{
-					bundle:   bundle,
-					manifest: manifest,
-				}
-			}
+			f.latestBoot = highestRelease(f.latestBoot, &firmwareRelease{bundle: bundle, manifest: manifest})
 		case ftlog.ComponentRecovery:
-			// See comment above about the Equal case.
-			if f.latestRecovery == nil ||
-				f.latestRecovery.manifest.GitTagName.LessThan(manifest.GitTagName) ||
-				f.latestRecovery.manifest.GitTagName.Equal(manifest.GitTagName) {
-				f.latestRecovery = &firmwareRelease{
-					bundle:   bundle,
-					manifest: manifest,
-				}
-			}
+			f.latestRecovery = highestRelease(f.latestRecovery, &firmwareRelease{bundle: bundle, manifest: manifest})
 		default:
 			klog.Warningf("unknown component type in log: %q", manifest.Component)
 		}
 	}
 	f.scanFrom = to.Size
 	return nil
+}
+
+// highestRelease returns the "higher" of the two releases passed in according to SemVer rules.
+//
+// According to the SemVer2.0 spec, equal revisions have no precedence defined.
+// In general this won't be an issue; production releases will always be tagged appropriately,
+// and there should never be two different releases with the same semver for a given component,
+// however, during development, this may not hold.
+// To tighten the definition of precedence, we'll use the fact that logs define ordering
+// and say that "later" entries take precedence over "earlier" entries with the same version
+// numbering.
+func highestRelease(current *firmwareRelease, candidate *firmwareRelease) *firmwareRelease {
+	if current == nil ||
+		current.manifest.GitTagName.LessThan(candidate.manifest.GitTagName) ||
+		current.manifest.GitTagName.Equal(candidate.manifest.GitTagName) {
+		return candidate
+	}
+	return current
 }
 
 func parseLeaf(leaf []byte, verifiers map[string][]note.Verifier) (ftlog.FirmwareRelease, error) {
